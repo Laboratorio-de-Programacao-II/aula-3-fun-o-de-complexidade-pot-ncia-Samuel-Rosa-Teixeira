5eab0727a117ebefdd31ea82b3061352aeb948b0
Samuel Rosa Teixeira <samuelrosa75@gmail.com>
2026-03-25 18:24:58 -0300

    atualizado main.c

--- main.c
+++ main.c
@@ -9,6 +9,19 @@ long long pot(long long base, int exp) {
 
 long long pot_eficiente(long long base, int exp) {
     /* IMPLEMENTAR ESTA FUNÇÃO COM ALGORITMO MAISEFICIENTE QUE POT() */
+    if(exp == 0)    //Se exp for 0
+    {
+        return 1;
+    }
+    else if (exp %2 == 0) //Se exp for par
+    {
+        int meio = pot_eficiente(base, exp/2);
+        return meio*meio;
+    }
+    else
+    {
+        return base*pot_eficiente(base, exp - 1);
+    }
     return 0; // Substitua este retorno pelo resultado correto
 }
 
